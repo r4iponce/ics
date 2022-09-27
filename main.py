91dc69bd95bb13a1d96b4ee8b565ae83dedf1176
r4iponce <99290572+r4iponce@users.noreply.github.com>
2022-09-27 15:35:14 +0200

    add verify_version()

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+from hashlib import sha256
+from os.path import exists
+from shutil import move
+
 import requests
 
-from config import TOKEN
+from config import DOWNLOAD_LOCATION, TOKEN
 
 
-def get_calendar(token: str) -> None:
-    headers = {'Authorization': token}
-    get = requests.get('https://portail.henallux.be/api/plannings/my/ical',
-                       headers=headers, timeout=5)
-    with open("calendar.ics", "wb") as file:
+def get_file_hash(file_to_hash: str) -> str:
+    with open(file_to_hash, "rb") as file:
+        return sha256(file.read()).hexdigest()
+
+
+def verify_version(file1: str, file2: str) -> bool:
+    return bool(get_file_hash(file1) == get_file_hash(file2))
+
+
+def download_calendar(token: str, download_path: str) -> None:
+    headers = {"Authorization": token}
+    get = requests.get(
+        "https://portail.henallux.be/api/plannings/my/ical", headers=headers, timeout=5
+    )
+    with open(download_path, "wb") as file:
         file.write(get.content)
-    print("Downloaded")
 
 
 if __name__ == "__main__":
-    get_calendar(TOKEN)
+    if exists(DOWNLOAD_LOCATION):
+        TEMP_DOWNLOAD_FILE = "temp_calendar.ics"
+        download_calendar(TOKEN, TEMP_DOWNLOAD_FILE)
+        if verify_version(DOWNLOAD_LOCATION, TEMP_DOWNLOAD_FILE):
+            print("No new version")
+        else:
+            move(TEMP_DOWNLOAD_FILE, DOWNLOAD_LOCATION)
+            print("New version downloaded")
+    else:
+        print("File downloaded")
+        download_calendar(TOKEN, DOWNLOAD_LOCATION)
